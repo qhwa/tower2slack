8663698b298dc1a97b0f7c58789c1a59c899d1c6
qhwa <qhwa@163.com>
2016-09-09 11:54:09 +0800

    重构

--- lib/tower2slack.ex
+++ lib/tower2slack.ex
@@ -1,6 +1,16 @@
 defmodule Tower2slack do
+
+  import Supervisor.Spec
   
   def start(:normal, args) do
+    children = [
+      worker(__MODULE__, [args])
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  def start_link(args) do
     Plug.Adapters.Cowboy.http(Tower2slack.Server, [], args)
   end
 
