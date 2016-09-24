623f7850cdf7a99d26fbe24d2aee4d70bc60e188
qhwa <qhwa@163.com>
2016-09-24 11:55:15 +0800

    优化代码

--- lib/tower2slack.ex
+++ lib/tower2slack.ex
@@ -3,11 +3,8 @@ defmodule Tower2slack do
   import Supervisor.Spec
   
   def start(:normal, args) do
-    children = [
-      worker(__MODULE__, [args])
-    ]
-
-    Supervisor.start_link(children, strategy: :one_for_one)
+    worker(__MODULE__, [args])
+      |> Supervisor.start_link(strategy: :one_for_one)
   end
 
   def start_link(args) do
