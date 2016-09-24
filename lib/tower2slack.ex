c60e9fd4460a58a7701b34ba872dfac058782095
qhwa <qhwa@163.com>
2016-09-24 16:05:21 +0800

    bugfix: 解决重构导致的bug

--- lib/tower2slack.ex
+++ lib/tower2slack.ex
@@ -1,9 +1,13 @@
 defmodule Tower2slack do
 
+  @moduledoc """
+  提供一个 HTTP 服务, 处理 [Tower.im](https://tower.im) 的 hook 数据，并转发给 Slack.
+  """
+
   import Supervisor.Spec
   
   def start(:normal, args) do
-    worker(__MODULE__, [args])
+    [worker(__MODULE__, [args])]
       |> Supervisor.start_link(strategy: :one_for_one)
   end
 
